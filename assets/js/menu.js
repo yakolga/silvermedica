55185fa99b07fefba485c6b9ec248e56299c3da8
Olga Kulczycka <olga.kulczycka07@gmail.com>
2023-02-19 19:28:59 +0100

    menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,15 +1,15 @@
 
-let isMobile = {
-  Android: function () { return navigator.userAgent.match(/Android/i); },
-  BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
-  iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
-  Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
-  Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
-  any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
-};
+// let isMobile = {
+//   Android: function () { return navigator.userAgent.match(/Android/i); },
+//   BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+//   iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+//   Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+//   Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+//   any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+// };
 
 let body = document.querySelector('body');
-if (isMobile.any()) {
+if (screen.width < 1200) {
   body.classList.add('touch');
   let arrow = document.querySelectorAll('.arrow');
   for (i = 0; i < arrow.length; i++) {
